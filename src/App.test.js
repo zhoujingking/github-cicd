f591fab525c0aa4c389b657d855cfbfc15136252
zhoujing01 <zhoujing01@jsfund.cn>
2023-07-04 16:37:17 +0800

    added a test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders logo', () => {
+  const { container } = render(<App />);
+  const logElem = container.querySelector('.App-logo');
+  expect(logElem).toBeInTheDocument();
+});
